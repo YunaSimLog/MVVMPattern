6ff4ad9a9c1087bf5d6389e655b9cf903357f13a
yunasim <ynsim@solidit.co.kr>
2024-01-17 00:34:11 +0900

    #✨feature: ViewModel 클래스 추가
    - 내부 상세 구현 진행 필요

---
+++ MVVMPattern/MVVMPattern/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMPattern.Models;
+
+namespace MVVMPattern.ViewModels
+{
+    public class MainViewModel
+    {
+        private readonly IBookReository _bookReository;
+
+        public MainViewModel(IBookReository bookReository)
+        {
+            _bookReository = bookReository;
+        }
+
+        public int ID { get; set; } = 1;
+        public string Title { get; set; } = "dd";
+        public string Writer { get; set; } = "cc";
+        public string RentalPeriod { get; set; } = "ddd";
+    }
+}
