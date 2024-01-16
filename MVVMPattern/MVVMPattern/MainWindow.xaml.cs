81bb8bd3e6248e962d0e19a89850c1500775fec8
yunasim <ynsim@solidit.co.kr>
2024-01-17 00:35:40 +0900

    #😎update: MVVM 패턴 버튼 클릭 시, View 보이게 작업
    - DataContext 부분으로 인해 View의 데이터가 ViewModel의 속성과 연결됨

--- MVVMPattern/MVVMPattern/MainWindow.xaml.cs
+++ MVVMPattern/MVVMPattern/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MVVMPattern.Models;
+using MVVMPattern.ViewModels;
+using MVVMPattern.Views;
 
 namespace MVVMPattern
 {
@@ -32,7 +35,13 @@ namespace MVVMPattern
 
         private void MVVMBtn_Click(object sender, RoutedEventArgs e)
         {
+            BookRepository bookRepository = new BookRepository();
+            MainView mainView = new MainView()
+            {
+                DataContext = new MainViewModel(bookRepository)
+            };
             
+            mainView.Show();
         }
     }
 }
